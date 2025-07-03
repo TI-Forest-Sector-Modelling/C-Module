eeb6c8da36346a97457b33af3b4d8be19b4998dd
honkomp <tomke.honkomp@gmail.com>
2025-07-03 16:45:18 +0200

    Add default_parameter

---
+++ C_Module/user_io/default_parameters.py
@@ -0,0 +1,34 @@
+# Overall parameters
+calc_c_ex_post = True
+calc_c_ex_ante = True
+
+start_year = 2020
+end_year = 2050
+
+read_in_pkl = True
+
+# Forest carbon related parameters
+calc_c_forest_agb = True
+calc_c_forest_bgb = True
+calc_c_forest_soil = True
+calc_c_forest_dwl = True
+
+# HWP carbon related parameters
+calc_c_hwp = True
+c_hwp_accounting_approach = "stock-change"  # Options: "stock-change" or "production"
+historical_c_hwp = "average"  # Options: "average" or "historical"
+
+user_input = {
+    "calc_c_ex_post": calc_c_ex_post,
+    "calc_c_ex_ante": calc_c_ex_ante,
+    "start_year": start_year,
+    "end_year": end_year,
+    "read_in_pkl": read_in_pkl,
+    "calc_c_forest_agb": calc_c_forest_agb,
+    "calc_c_forest_bgb": calc_c_forest_bgb,
+    "calc_c_forest_soil": calc_c_forest_soil,
+    "calc_c_forest_dwl": calc_c_forest_dwl,
+    "calc_c_hwp": calc_c_hwp,
+    "c_hwp_accounting_approach": c_hwp_accounting_approach,
+    "historical_c_hwp": historical_c_hwp
+}
