ac0314f4645a6cab0c962849800ed8cd36a18e5e
tomke honkomp <tomke.honkomp@thuenen.de>
2025-08-11 14:54:41 +0200

    Delete target folder

--- C_Module/main.py
+++
@@ -1,8 +0,0 @@
-from c_module.logic.main import C_Module
-from c_module.user_io.default_parameters import user_input
-
-
-if __name__ == "__main__":
-    c_module = C_Module(UserInput=user_input)
-    c_module.run()
-
