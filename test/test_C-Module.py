b57ddff9393a0f5182e1c7ec7a8583fe48b2a856
tomke honkomp <tomke.honkomp@thuenen.de>
2025-08-11 14:50:49 +0200

    Revise folder name

--- test/test_C-Module.py
+++ test/test_C-Module.py
@@ -1,7 +1,7 @@
 import unittest
 
-from tmp_folder.logic.main import C_Module
-from tmp_folder.user_io.default_parameters import user_input
+from c_module.logic.main import C_Module
+from c_module.user_io.default_parameters import user_input
 
 
 class TestCModuleClass(unittest.TestCase):
--- tmp_folder/main.py
+++
@@ -1,8 +0,0 @@
-from tmp_folder.logic.main import C_Module
-from tmp_folder.user_io.default_parameters import user_input
-
-
-if __name__ == "__main__":
-    c_module = C_Module(UserInput=user_input)
-    c_module.run()
-
